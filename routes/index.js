14f6c0fdde7cd5fe5d506e0232e7a5428547b0a1
rickshaw68 <rickshaw@myyahoo.com>
2025-11-22 17:34:12 -0700

    Added Google OAuth (auth.js, passport.js, user model) and updated server.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// Auth
+router.use('/auth', require('./auth'));
+
 // Users
 // router.use('/users', require('./users'));
 
--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const mongodb = require('./db/connect');
+const passport = require('./config/passport');
 
-const port = process.env.PORT || 3000;
+dotenv.config();
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 app
     .use(express.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, DELETE, OPTIONS'
+        );
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
         next();
     })
+
+    .use(
+        session({
+            secret: process.env.SESSION_SECRET || '9258da7e79295d2c76d0b3237af30aab',
+            resave: false,
+            saveUninitialized: false,
+        })
+    )
+    
+    .use(passport.initialize())
+    .use(passport.session())
+
     .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
