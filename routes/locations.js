a25ce52967ed6b1b8055da0e61157d25a0b9c6ce
Rick Shaw <rickshaw@myyahoo.com>
2025-12-02 16:30:05 -0700

    Add OAuth protection for Locations, add logout route, update Swagger login/logout, add validation updates, add Jest tests

--- routes/locations.js
+++ routes/locations.js
@@ -4,19 +4,25 @@ const router = require('express').Router();
 
 const locationsController = require('../controllers/locations');
 
+// OAuth protection
+const { ensureAuth } = require('../middleware/auth')
+
+// ****Unprotected routes****
 //get locations
 router.get('/', locationsController.getAll);
 
 // get locations by ID
 router.get('/:id', locationsController.getOne);
 
+
+// ****Protected routes ****
 // post a new location
-router.post('/', locationsController.createOne);
+router.post('/', ensureAuth, locationsController.createOne);
 
 // modify an existing location by ID
-router.put('/:id', locationsController.updateOne);
+router.put('/:id', ensureAuth, locationsController.updateOne);
 
 // delete a location by ID
-router.delete('/:id', locationsController.deleteOne);
+router.delete('/:id', ensureAuth, locationsController.deleteOne);
 
 module.exports = router;
